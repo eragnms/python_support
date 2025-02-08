2fb386f0ed4f3246d4f1afd9bd1846f53f3dfd3b
Mats Gustafsson <mats@mcnab.se>
2025-02-08 12:17:47 +0100

    Add cli app for pushover

--- python_support/pushover_message.py
+++ python_support/pushover_message.py
@@ -2,9 +2,15 @@
 
 
 This module contains the function to send a message to the Pushover server.
+It can also be installed as a command line tool to send messages from the terminal.
+Install with `pipx install .` and use with `pushover_message <message>`. In case of
+running the script from the cli PUSHOVER_APP_TOKEN and PUSHOVER_USER_KEY environment
+variables must be set.
 """
 
 import http.client
+import os
+import sys
 import urllib.parse
 
 
@@ -15,7 +21,6 @@ class PushoverMessage:
     Attributes:
         app_token: The Pushover application token.
         user_key: The Pushover user key.
-
     """
 
     def __init__(self, app_token: str, user_key: str) -> None:
@@ -40,3 +45,32 @@ class PushoverMessage:
             {"Content-type": "application/x-www-form-urlencoded"},
         )
         conn.getresponse()
+
+
+def main() -> None:
+    # Read environment variables for app_token and user_key
+    app_token = os.getenv("PUSHOVER_APP_TOKEN")
+    user_key = os.getenv("PUSHOVER_USER_KEY")
+
+    if not app_token or not user_key:
+        print(
+            "Error: PUSHOVER_APP_TOKEN or PUSHOVER_USER_KEY "
+            "environment variables are not set."
+        )
+        sys.exit(1)
+
+    # Ensure a message is passed as a CLI argument
+    if len(sys.argv) < 2:
+        print("Usage: pushover-message <message>")
+        sys.exit(1)
+
+    message = sys.argv[1]
+
+    # Create PushoverMessage instance and send the message
+    pushover = PushoverMessage(app_token, user_key)
+    pushover.send(message)
+    print("Pushover message sent successfully!")
+
+
+if __name__ == "__main__":
+    main()
